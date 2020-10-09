fac5a16fc62c6d46c21a02d535e925574c0d2713
Nicholas Merolle <nmerolle11@hotmail.com>
2020-10-09 07:10:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,6 +53,13 @@ class Song
     song
   end
   
-  
+  def self.create_from_filename(name)
+    song
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    @@all << song
+    song
+  end
   
   end

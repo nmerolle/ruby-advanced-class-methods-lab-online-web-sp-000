0c5e5ebdddf77e1d12eb44715669cd0e52cd4972
Nicholas Merolle <nmerolle11@hotmail.com>
2020-10-09 07:04:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,31 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new   
+    song.name = name
+    song
+  end
 
+  def self.create_by_name(name)
+    song = self.new 
+    song.name = name
+    @@all << song
+    song
+  end
+  
+  def self.find_by_name(name)
+    @@all.find{|x| x.name == name}
+  end
+  
+  
+  
+  
 end
